1b64e1a2ea62786011919d2f3b8a1adafdf10636
kaschnit <kevinschnitzer@gmail.com>
2023-04-23 12:14:55 -0400

    Add test coverage

--- pkg/compare/hashkey.go
+++ pkg/compare/hashkey.go
@@ -1,7 +1,11 @@
 package compare
 
+// HashKey is used to convert a potentially non-comparable value to a comparable value.
+// This is useful for computing hash keys for a hash map, for example.
 type HashKey[K any, H comparable] func(key K) H
 
+// IdentityHashKey is a no-op on a value that's already comparable value.
+// This allows use of the value itself as the hash key with interfaces that require a HashKey.
 func IdentityHashKey[K comparable](key K) K {
 	return key
 }
--- pkg/compare/hashkey_test.go
+++ pkg/compare/hashkey_test.go
@@ -1,7 +1,46 @@
 package compare_test
 
-import "github.com/kaschnit/go-ds/pkg/compare"
+import (
+	"testing"
+
+	"github.com/kaschnit/go-ds/pkg/compare"
+	"github.com/stretchr/testify/assert"
+)
 
 // Ensure that OrderedComparator implements Comparator
 var _ compare.HashKey[int, int] = compare.IdentityHashKey[int]
 var _ compare.HashKey[string, string] = compare.IdentityHashKey[string]
+
+func TestIdentityHashKey(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    any
+		expected any
+	}{
+		{
+			name:     "numeric input 1",
+			input:    1,
+			expected: 1,
+		},
+		{
+			name:     "numeric input 2",
+			input:    -8000,
+			expected: -8000,
+		},
+		{
+			name:     "string input 1",
+			input:    "",
+			expected: "",
+		},
+		{
+			name:     "string input 2",
+			input:    "foo",
+			expected: "foo",
+		},
+	}
+	for _, testCase := range tests {
+		t.Run(testCase.name, func(t *testing.T) {
+			assert.Equal(t, testCase.expected, compare.IdentityHashKey(testCase.input))
+		})
+	}
+}
